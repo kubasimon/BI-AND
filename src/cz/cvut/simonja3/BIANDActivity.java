9e326297c5ff1190264a4a964ec7f8c7488e3878
Jakub Šimon <simonja3@fit.cvut.cz>
2012-03-25 19:37:34 +0200

    search aktivita

--- src/cz/cvut/simonja3/BIANDActivity.java
+++ src/cz/cvut/simonja3/BIANDActivity.java
@@ -1,7 +1,9 @@
 package cz.cvut.simonja3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BIANDActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,10 @@ public class BIANDActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void switchToSearch(View view) {
+    	Intent intent = new Intent(view.getContext(), SearchActivity.class);
+    	this.startActivity(intent);
     }
 }
